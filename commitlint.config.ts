e128d783eb69f2513b2142e84f71896b04739f72
Serhii Chernenko <serhii.chernenko@vaimo.com>
2026-04-05 13:23:48 +0200

    chore: code quality tools installed

---
+++ commitlint.config.ts
@@ -0,0 +1,33 @@
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  formatter: "@commitlint/format",
+  rules: {
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "security",
+        "legal",
+        "kludge",
+        "ai",
+      ],
+    ],
+    "subject-case": [RuleConfigSeverity.Error, "never", ["upper-case"]],
+  },
+};
+
+export default Configuration;
